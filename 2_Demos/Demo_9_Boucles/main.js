3d33e27e4be73280d1fa76d1adfd3584d05abfe4
Caroline Poveda <c.poveda@interface3.be>
2025-10-09 14:09:41 +0200

    Terminé exercice 6 + début Démo 9

---
+++ 2_Demos/Demo_9_Boucles/main.js
@@ -0,0 +1,18 @@
+//!Les boucles
+//! 2 types :
+
+//?TANT QUE (condition ok) FAIRE (quelque-chose)
+//? while(condition){}
+
+//?FAIRE (quelque-chose)) TANT QUE (condition ok)
+//? do{}while(condition)
+// = vaut pour une action dont on ne sait pas combien de fois elle va se répéter
+// Ex : un jeu, la lecture d'un fichier...
+
+//?----------------------------------------------
+//? POUR VALEUR ALLANT DE X À Y, PART PAS DE Z, FAIRE
+//? for(let i = 0; i <10, i++) {}
+// = vaut pour une action dont on sait exactement combien de fois elle va se répéter.
+// Ex : action à faire sur un tablau de 4 cases -> action répétée 4 fois.
+
+//?--------------------------------------------------
--- 3_Exercices/Exercice_6_Tableaux/main.js
+++ 3_Exercices/Exercice_6_Tableaux/main.js
@@ -6,31 +6,60 @@ const P_INVALIDE = document.getElementById('invalide');
 const P_AFFICHAGE = document.getElementById('affichage');
 const BTN_TIRAGE = document.getElementById('tirage');
 const P_AFFICHAGE_TIR = document.getElementById('affichageTirage');
+const BTN_RESET = document.getElementById('reset');
 
 const TAB_PRENOM = [];
 
 BTN_AJOUTER.addEventListener('click', () => {
-    let inputPrenom = INPUT_PRENOM.value;
+    addName();
+});
+
+INPUT_PRENOM.addEventListener('keydown', (e) => {
+    if(e.key === 'Enter'){
+        addName();
+    }
+});
+
+function addName() {
+    let inputPrenom = INPUT_PRENOM.value.trim(); 
+    //* trim = sert à faire disparaître les espaces avant ou après une chaine de caractère => ne compte pas l'espace comme un caractère.
     if(inputPrenom == "" || !isNaN(inputPrenom)) {
         P_INVALIDE.textContent = "Attention, tu dois rentrer une chaine de caractères !";
         INPUT_PRENOM.value = "";
     } else {
         TAB_PRENOM.push(inputPrenom); // Pour entrer une valeur liée à un élément html, écrire directement le nom de sa variable entre () et sans '' !
         P_AFFICHAGE.textContent = TAB_PRENOM.join(' | ');
+        INPUT_PRENOM.value = "";
     }
-});
+};
 
-
-// Étape 2 : Random, afficher case en particulier, suppression
+// Étape 2 : Random, afficher case en particulier
 
 let random;
-let tabLength = TAB_PRENOM.length;
 
 BTN_TIRAGE.addEventListener('click', () => {
-    random = Math.floor(Math.random() * tabLength + 0);
-    if( random == tabLength ) { //! ça ne peut pas être == tabLength, car tabLength = longueur du tableau, PAS l'index d'un nom !
-        P_AFFICHAGE_TIR.textContent = `Pas de bol ${}`;
-    }
-
+    randomName()
 });
+
+function randomName() {
+    let tabLength = TAB_PRENOM.length; // * tabLength doit être à l'INTERIEUR de l'évènement, sinon elle ne se calcule qu'une fois avant même le clic et elle est toujours égale à 1 => n'affiche que le premier prénom à chq fois !
+    random = Math.floor(Math.random() * tabLength);
+    P_AFFICHAGE_TIR.textContent = `Bonté divine ${TAB_PRENOM[random]}, Père a perdu tous ses bitcoins, nous allons devoir vendre la maison du Cap d'Agde 😭 !`
+    
+// Étape 3 : suppression du nom sélectionné
+    TAB_PRENOM.splice(random, 1);
+    P_AFFICHAGE.textContent = TAB_PRENOM.join(' | ');
+};
+
+BTN_RESET.addEventListener('click', () => {
+    TAB_PRENOM.length = 0;
+    P_AFFICHAGE.textContent = '';
+    P_AFFICHAGE_TIR.textContent = '';
+});
+
+
+
+
+
+
 
